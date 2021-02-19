fd7a01ea56d708d0138500e709d94e216ab549a2
Lateralus <zoofoo.online@gmail.com>
2021-02-19 20:34:19 +0000

    added types and actions for singup

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -2,8 +2,11 @@ import { AUTHENTICATE, SIGN_IN, SIGN_OUT, SIGN_UP } from './types';
 
 export const signIn = () => ({ type: SIGN_IN });
 export const signOut = () => ({ type: SIGN_OUT });
-export const signUp = () => ({ type: SIGN_UP });
 export const authenticate = (email, password) => ({
   type: AUTHENTICATE,
   payload: { email, password },
+});
+export const signUp = (email, name, surname, password) => ({
+  type: SIGN_UP,
+  payload: { email, name, surname, password },
 });
