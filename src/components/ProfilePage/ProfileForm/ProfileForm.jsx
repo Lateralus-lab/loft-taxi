039d926cce0676abe116ea59f05efee2f351b870
Lateralus <zoofoo.online@gmail.com>
2021-03-07 20:19:40 +0000

    started adding card styles

--- src/components/ProfilePage/ProfileForm/ProfileForm.jsx
+++ src/components/ProfilePage/ProfileForm/ProfileForm.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography } from '@material-ui/core';
+import LogoCard from '../../svg/LogoCard';
+import IdCard from '../../svg/IdCard';
+import LabelCard from '../../svg/LabelCard';
 
 const ProfileForm = ({ cardData }) => {
   const [cardHolder, setCardHolder] = useState('JOHN SMITH');
@@ -15,7 +18,7 @@ const ProfileForm = ({ cardData }) => {
 
   return (
     <form
-      className="form"
+      className="form form--profile"
       noValidate
       autoComplete="off"
       onSubmit={handleSubmit}
@@ -26,51 +29,70 @@ const ProfileForm = ({ cardData }) => {
         </Typography>
       </div>
       <div className="form__desc">Please fill out the form</div>
-      <div className="input__group">
-        <TextField
-          className="input"
-          data-testid="name"
-          label="Name on card"
-          required
-          value={cardHolder}
-          onChange={(e) => setCardHolder(e.target.value)}
-        />
-      </div>
-      <div className="input__group">
-        <TextField
-          className="input"
-          data-testid="card-number"
-          label="Card number"
-          required
-          value={cardNumber}
-          onChange={(e) => setCardNumber(e.target.value)}
-        />
-      </div>
-      <div className="input__group input__group--double">
-        <TextField
-          className="input input--small"
-          data-testid="expiry"
-          name="expiritation"
-          variant="standard"
-          label="MM/YY"
-          required
-          inputProps={{ maxLength: 5 }}
-          value={expiryDate}
-          onChange={(e) => setExpiryDate(e.target.value)}
-        />
-        <TextField
-          className="input input--small"
-          data-testid="cvc"
-          variant="standard"
-          label="CVC"
-          required
-          inputProps={{ maxLength: 3 }}
-          value={cvc}
-          onChange={(e) => setCvc(e.target.value)}
-        />
+      <div className="profile">
+        <div profile__left>
+          <div className="input__group">
+            <TextField
+              className="input"
+              data-testid="name"
+              label="Name on card"
+              required
+              value={cardHolder}
+              onChange={(e) => setCardHolder(e.target.value)}
+            />
+          </div>
+          <div className="input__group">
+            <TextField
+              className="input"
+              data-testid="card-number"
+              label="Card number"
+              required
+              value={cardNumber}
+              onChange={(e) => setCardNumber(e.target.value)}
+            />
+          </div>
+          <div className="input__group input__group--double">
+            <TextField
+              className="input input--small"
+              data-testid="expiry"
+              name="expiritation"
+              variant="standard"
+              label="MM/YY"
+              required
+              inputProps={{ maxLength: 5 }}
+              value={expiryDate}
+              onChange={(e) => setExpiryDate(e.target.value)}
+            />
+            <TextField
+              className="input input--small"
+              data-testid="cvc"
+              variant="standard"
+              label="CVC"
+              required
+              inputProps={{ maxLength: 3 }}
+              value={cvc}
+              onChange={(e) => setCvc(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="profile__right">
+          <div className="card">
+            <div className="card__header">
+              <LogoCard />
+              <div className="card__expiry">05/24</div>
+            </div>
+            Card
+            <IdCard />
+            <LabelCard />
+          </div>
+        </div>
       </div>
       <div>
-        <Button className="button" data-testid="save-button" type="submit">
+        <Button
+          className="button button--profile"
+          data-testid="save-button"
+          type="submit"
+        >
           Save
         </Button>
       </div>
---
+++ src/components/svg/LabelCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function LabelCard() {
+  return (
+    <svg
+      width="46"
+      height="28"
+      viewBox="0 0 46 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <circle cx="14" cy="14" r="14" fill="#F1F1F1" />
+      </g>
+      <g>
+        <circle cx="32" cy="14" r="14" fill="#F1F1F1" />
+      </g>
+    </svg>
+  );
+}
+
+export default LabelCard;
