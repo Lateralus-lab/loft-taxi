e7b8410ee8903a8f4e4f73aae1937b8c77810031
Lateralus <zoofoo.online@gmail.com>
2021-03-10 17:40:34 +0000

    fixed card footer styling with flex box

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ProfileFormContainer from './ProfileForm/ProfileFormContainer';
-// import CardSaved from './CardSaved/CardSaved';
 
 const ProfilePage = () => {
   return (
     <div className="profilepage">
       <ProfileFormContainer />
-      {/* <CardSaved /> */}
     </div>
   );
 };
